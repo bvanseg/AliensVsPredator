88d0fb608a5c65cbb231f5a35b552e5974587e69
Dustin Christensen <Ri5ux@users.noreply.github.com>
2017-06-11 23:18:50 -0500

    Fixed the rendering of stalagmites

--- src/main/java/org/avp/block/BlockStalagmite.java
+++ src/main/java/org/avp/block/BlockStalagmite.java
@@ -4,20 +4,13 @@ import java.util.Random;
 
 import org.avp.AliensVsPredator;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumBlockRenderType;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.property.IUnlistedProperty;
 
 public class BlockStalagmite extends Block
 {
@@ -25,38 +18,6 @@ public class BlockStalagmite extends Block
     {
         super(material);
         this.setTickRandomly(true);
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[0])
-        {
-            @Override
-            protected StateImplementation createState(Block block, ImmutableMap<IProperty<?>, Comparable<?>> properties, ImmutableMap<IUnlistedProperty<?>, Optional<?>> unlistedProperties)
-            {
-                return new StateImplementation(block, properties)
-                {
-                    @Override
-                    public EnumBlockRenderType getRenderType()
-                    {
-                        return EnumBlockRenderType.INVISIBLE;
-                    }
-
-                    @Override
-                    public boolean isOpaqueCube()
-                    {
-                        return false;
-                    }
-                    
-                    @Override
-                    public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos)
-                    {
-                        return null;
-                    }
-                };
-            }
-        };
     }
 
     @Override
@@ -100,5 +61,17 @@ public class BlockStalagmite extends Block
     public boolean canBlockStay(IBlockAccess world, BlockPos pos)
     {
         return this.canThisPlantGrowOn(world.getBlockState(pos.add(0, -1, 0)));
+    }
+    
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+    
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
     }
 }
