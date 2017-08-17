01b3f7ce1b33de457c478e1dca8ac00ae5761cf9
Ri5ux <Ri5ux@users.noreply.github.com>
2017-08-17 01:14:06 -0500

    Fixed a glitch where the fog effect of mist would not be removed after leaving the mist.

--- src/main/java/org/avp/client/render/materials/RenderMaterialMist.java
+++ src/main/java/org/avp/client/render/materials/RenderMaterialMist.java
@@ -4,6 +4,7 @@ import org.avp.api.blocks.material.IMaterialRenderer;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.math.Vec3d;
 
 
@@ -24,7 +25,7 @@ public class RenderMaterialMist implements IMaterialRenderer
     @Override
     public void renderFog(Material material)
     {
-        GL11.glFogi(GL11.GL_FOG_MODE, GL11.GL_EXP);
+        GlStateManager.setFog(GlStateManager.FogMode.EXP);
         GL11.glFogf(GL11.GL_FOG_DENSITY, 0.2F);
     }
 }
