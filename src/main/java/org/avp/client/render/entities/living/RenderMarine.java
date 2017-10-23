7c97255c0f362f1abc32f89ae751eb2505821430
Ri5ux <Ri5ux@users.noreply.github.com>
2017-10-23 03:20:13 -0500

    Fixed the marine item renderer.

--- src/main/java/org/avp/client/render/entities/living/RenderMarine.java
+++ src/main/java/org/avp/client/render/entities/living/RenderMarine.java
@@ -4,70 +4,98 @@ import org.avp.AliensVsPredator;
 import org.avp.client.model.entities.living.ModelMarine;
 import org.avp.entities.living.EntityMarine;
 
-import com.arisux.mdx.lib.client.Model;
 import com.arisux.mdx.lib.client.RenderLivingWrapper;
-import com.arisux.mdx.lib.client.TexturedModel;
-import com.arisux.mdx.lib.client.render.OpenGL;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBiped.ArmPose;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHandSide;
 
 public class RenderMarine extends RenderLivingWrapper<EntityMarine, ModelMarine>
 {
+    public static class LayerHeldGun extends LayerHeldItem
+    {
+        public LayerHeldGun(RenderLivingBase<?> livingEntityRendererIn)
+        {
+            super(livingEntityRendererIn);
+        }
+
+        @Override
+        public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+        {
+            boolean flag = entitylivingbaseIn.getPrimaryHand() == EnumHandSide.RIGHT;
+            ItemStack itemstack = flag ? entitylivingbaseIn.getHeldItemOffhand() : entitylivingbaseIn.getHeldItemMainhand();
+            ItemStack itemstack1 = flag ? entitylivingbaseIn.getHeldItemMainhand() : entitylivingbaseIn.getHeldItemOffhand();
+
+            if (itemstack != null || itemstack1 != null)
+            {
+                GlStateManager.pushMatrix();
+
+                if (this.livingEntityRenderer.getMainModel().isChild)
+                {
+                    GlStateManager.translate(0.0F, 0.625F, 0.0F);
+                    GlStateManager.rotate(-20.0F, -1.0F, 0.0F, 0.0F);
+                    GlStateManager.scale(0.5F, 0.5F, 0.5F);
+                }
+
+                this.renderHeldItem(entitylivingbaseIn, itemstack1, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, EnumHandSide.RIGHT);
+                this.renderHeldItem(entitylivingbaseIn, itemstack, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, EnumHandSide.LEFT);
+                GlStateManager.popMatrix();
+            }
+        }
+
+        private void renderHeldItem(EntityLivingBase living, ItemStack stack, ItemCameraTransforms.TransformType transformType, EnumHandSide handSide)
+        {
+            if (this.livingEntityRenderer instanceof RenderLivingWrapper && living instanceof EntityMarine)
+            {
+                EntityMarine marine = (EntityMarine) living;
+                RenderLivingWrapper<EntityMarine, ModelMarine> renderer = (RenderLivingWrapper<EntityMarine, ModelMarine>) this.livingEntityRenderer;
+
+                if (stack != null)
+                {
+                    GlStateManager.pushMatrix();
+
+                    if (living.isSneaking())
+                    {
+                        GlStateManager.translate(0.0F, 0.2F, 0.0F);
+                    }
+
+                    if (marine.isFiring())
+                    {
+                        renderer.getModel().getModel().leftArmPose = ArmPose.BOW_AND_ARROW;
+                    }
+                    else
+                    {
+                        renderer.getModel().getModel().leftArmPose = ArmPose.EMPTY;
+                    }
+
+                    renderer.getModel().getModel().postRenderArm(0.0625F, handSide);
+                    GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+                    GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+                    boolean flag = handSide == EnumHandSide.LEFT;
+                    GlStateManager.translate((float) (flag ? -1 : 1) / 16.0F, 0.125F, -0.625F);
+                    Minecraft.getMinecraft().getItemRenderer().renderItemSide(living, stack, transformType, flag);
+                    GlStateManager.popMatrix();
+                }
+            }
+        }
+    }
+
     public RenderMarine(RenderManager m)
     {
         super(m, AliensVsPredator.resources().models().MARINE);
+        this.addLayer(new LayerHeldGun(this));
     }
 
     @Override
     protected void preRenderCallback(EntityMarine living, float partialTicks)
     {
         super.preRenderCallback(living, partialTicks);
-    }
-    
-    protected void renderEquippedItems(EntityMarine living, float partialTicks)
-    {
-        //super.renderEquippedItems(living, partialTicks);
-
-        EntityMarine marine = (EntityMarine) living;
-        TexturedModel<? extends Model> model = marine.getMarineType().getFirearmModelTexMap();
-
-        OpenGL.pushMatrix();
-        {
-            this.getModel().getModel().bipedRightArm.postRender(Model.DEFAULT_SCALE);
-            if (marine.isFiring())
-            {
-                this.getModel().getModel().aimedBow = true;
-            }
-            else
-            {
-                this.getModel().getModel().aimedBow = false;
-            }
-            OpenGL.translate(-0.35F, 0.8F, -0.85F);
-            OpenGL.rotate(270.0F, 1.0F, 0.0F, 0.0F);
-            OpenGL.rotate(0.0F, 0.0F, 1.0F, 0.0F);
-            OpenGL.rotate(180.0F, 0.0F, 0.0F, 1.0F);
-            OpenGL.scale(1.2F, 1.2F, 1.2F);
-            GlStateManager.disableCull();
-
-            switch (marine.getMarineType())
-            {
-                case AK47:
-                    OpenGL.translate(-0.35F, 0.45F, -0.55F);
-                    break;
-                case SNIPER:
-                    OpenGL.translate(-0.25F, 0.45F, 0.05F);
-                    break;
-                case M56SG:
-                    OpenGL.translate(-0.15F, 0.7F, -0.73F);
-                    this.getModel().getModel().aimedBow = false;
-                    break;
-                default:
-                    break;
-            }
-
-            model.draw();
-        }
-        OpenGL.popMatrix();
     }
 }
