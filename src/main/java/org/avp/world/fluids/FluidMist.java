ccd51295ca4a3bb701e23e9726cd6abfcaef86f4
Ri5ux <Ri5ux@users.noreply.github.com>
2018-09-27 22:30:42 -0500

    Fixed mist texture

--- src/main/java/org/avp/world/fluids/FluidMist.java
+++ src/main/java/org/avp/world/fluids/FluidMist.java
@@ -1,13 +1,15 @@
 package org.avp.world.fluids;
 
+import org.avp.AliensVsPredator;
+
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class FluidMist extends Fluid
 {
     //TODO: Flowing, Still texture locations
-    public static final ResourceLocation flowing = new ResourceLocation("items.blocks/mist.flowing");
-    public static final ResourceLocation still = new ResourceLocation("items.blocks/mist.still");
+    public static final ResourceLocation flowing = new ResourceLocation(AliensVsPredator.Properties.ID, "blocks/mist.flowing");
+    public static final ResourceLocation still = new ResourceLocation(AliensVsPredator.Properties.ID, "blocks/mist.still");
     
     public FluidMist()
     {
