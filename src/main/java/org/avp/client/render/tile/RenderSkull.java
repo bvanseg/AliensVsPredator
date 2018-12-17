4422e056ae316a3bddb5de80404bac8f921602e5
Dustin Christensen <Ri5ux@users.noreply.github.com>
2018-12-17 17:49:09 -0600

    Fix for an ocassional crash caused by skulls.

--- src/main/java/org/avp/client/render/tile/RenderSkull.java
+++ src/main/java/org/avp/client/render/tile/RenderSkull.java
@@ -16,44 +16,50 @@ public class RenderSkull extends TileEntitySpecialRenderer<TileEntitySkull>
     @Override
     public void render(TileEntitySkull skull, double posX, double posY, double posZ, float renderPartialTicks, int destroyStage, float alpha)
     {
-        BlockSkull block = (BlockSkull) skull.getBlockType();
-
-        OpenGL.pushMatrix();
-        float scale = 0.64F;
-
-        OpenGL.translate(posX + 0.5F, posY, posZ + 0.5F);
-        OpenGL.scale(scale, -scale, scale);
-        OpenGL.enable(GL11.GL_ALPHA_TEST);
-        OpenGL.disableCullFace();
-        OpenGL.rotate(skull);
-
-        if (skull.getSkullBlock().getSkullTexture() != null)
+        if (skull != null)
         {
-            skull.getSkullBlock().getSkullTexture().bind();
-        }
+            BlockSkull block = (BlockSkull) skull.getBlockType();
 
-        skull.getSkullBlock().preRenderTransforms();
+            OpenGL.pushMatrix();
+            float scale = 0.64F;
 
-        if (block.canChangeOrientation())
-        {
-            if (skull.getOrientation() == EnumOrientation.FLAT)
+            OpenGL.translate(posX + 0.5F, posY, posZ + 0.5F);
+            OpenGL.scale(scale, -scale, scale);
+            OpenGL.enable(GL11.GL_ALPHA_TEST);
+            OpenGL.disableCullFace();
+            OpenGL.rotate(skull);
+
+            if (skull.getSkullBlock() != null)
             {
-                OpenGL.translate(-0.4, 0.71F, 0);
-                OpenGL.rotate(-53.5F, 0, 0, 1);
-            }
+                if (skull.getSkullBlock().getSkullTexture() != null)
+                {
+                    skull.getSkullBlock().getSkullTexture().bind();
+                }
 
-            if (skull.getOrientation() == EnumOrientation.WALL)
-            {
-                OpenGL.translate(-0.25, -0.1, 0);
-                OpenGL.rotate(37.5F, 0, 0, 1);
-            }
-        }
+                skull.getSkullBlock().preRenderTransforms();
 
-        for (ModelRenderer m : skull.getSkullBlock().getSkullModelRenderers())
-        {
-            m.render(Model.DEFAULT_SCALE);
+                if (block.canChangeOrientation())
+                {
+                    if (skull.getOrientation() == EnumOrientation.FLAT)
+                    {
+                        OpenGL.translate(-0.4, 0.71F, 0);
+                        OpenGL.rotate(-53.5F, 0, 0, 1);
+                    }
+
+                    if (skull.getOrientation() == EnumOrientation.WALL)
+                    {
+                        OpenGL.translate(-0.25, -0.1, 0);
+                        OpenGL.rotate(37.5F, 0, 0, 1);
+                    }
+                }
+
+                for (ModelRenderer m : skull.getSkullBlock().getSkullModelRenderers())
+                {
+                    m.render(Model.DEFAULT_SCALE);
+                }
+            }
+            OpenGL.enableCullFace();
+            OpenGL.popMatrix();
         }
-        OpenGL.enableCullFace();
-        OpenGL.popMatrix();
     }
 }
