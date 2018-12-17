8103be7fa3355ba03c24cd875278e6d8678a31a3
Dustin Christensen <Ri5ux@users.noreply.github.com>
2018-12-17 02:02:18 -0600

    Fixed ore generation
    Lithium now generates much more commonly under oceans and rivers.
    Fixed silicon generation (Seemed to not work)
    Slightly increased the spawn rates of other ores.

--- src/main/java/org/avp/world/worldgen/WorldGenerator.java
+++ src/main/java/org/avp/world/worldgen/WorldGenerator.java
@@ -2,7 +2,7 @@ package org.avp.world.worldgen;
 
 import java.util.Random;
 
-import org.avp.AliensVsPredator;
+import org.avp.BlockHandler;
 
 import com.arisux.mdx.lib.world.Worlds;
 
@@ -30,19 +30,19 @@ public class WorldGenerator implements IWorldGenerator
         Biome[] overworldBiomes = new Biome[] { Biomes.BEACH, Biomes.BIRCH_FOREST, Biomes.BIRCH_FOREST_HILLS, Biomes.COLD_BEACH, Biomes.COLD_TAIGA, Biomes.COLD_TAIGA_HILLS, Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.EXTREME_HILLS, Biomes.EXTREME_HILLS_EDGE, Biomes.EXTREME_HILLS_WITH_TREES, Biomes.FOREST, Biomes.FOREST_HILLS, Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER, Biomes.ICE_MOUNTAINS, Biomes.ICE_PLAINS, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.PLAINS, Biomes.RIVER, Biomes.ROOFED_FOREST, Biomes.SWAMPLAND, Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.OCEAN, Biomes.DEEP_OCEAN
         };
         
-        Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreBauxite.getDefaultState(), 4), rand, 20, 16, 128, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreCopper.getDefaultState(), 4), rand, 20, 0, 128, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreSilicon.getDefaultState(), 1), rand, 15, 0, 64, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreMonazite.getDefaultState(), 3), rand, 4, 1, 48, chunkCoords, overworldBiomes);
+        Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreBauxite.getDefaultState(), 4), rand, 20, 16, 128, chunkCoords, overworldBiomes);
+        Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreCopper.getDefaultState(), 4), rand, 20, 0, 128, chunkCoords, overworldBiomes);
+        Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreSilicon.getDefaultState(), 3), rand, 4, 1, 64, chunkCoords, overworldBiomes);
+        Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreMonazite.getDefaultState(), 3), rand, 4, 1, 48, chunkCoords, overworldBiomes);
         
         if (biome == Biomes.BEACH || biome == Biomes.OCEAN || biome == Biomes.DEEP_OCEAN || biome == Biomes.FROZEN_OCEAN || biome == Biomes.FROZEN_RIVER)
         {
-            Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreLithium.getDefaultState(), 6), rand, 1, 1, 48, chunkCoords, overworldBiomes);
+            Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreLithium.getDefaultState(), 12), rand, 1, 1, 48, chunkCoords, overworldBiomes);
         }
         
         if (biome == Biomes.JUNGLE || biome == Biomes.JUNGLE_EDGE || biome == Biomes.JUNGLE_HILLS || biome == Biomes.MUTATED_JUNGLE || biome == Biomes.MUTATED_JUNGLE_EDGE)
         {
-            Worlds.generateInBiome(world, new WorldGenMinable(AliensVsPredator.blocks().oreCobalt.getDefaultState(), 6), rand, 1, 1, 64, chunkCoords, overworldBiomes);
+            Worlds.generateInBiome(world, new WorldGenMinable(BlockHandler.oreCobalt.getDefaultState(), 6), rand, 1, 1, 64, chunkCoords, overworldBiomes);
         }
     }
 }
