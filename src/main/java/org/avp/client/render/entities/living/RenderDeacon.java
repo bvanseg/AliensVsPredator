904a6438de985e7ac918992179d458c99a402665
Dustin Christensen <Ri5ux@users.noreply.github.com>
2019-03-12 21:59:24 -0500

    Deacon renderers no longer extend RenderXenomorph

--- src/main/java/org/avp/client/render/entities/living/RenderDeacon.java
+++ src/main/java/org/avp/client/render/entities/living/RenderDeacon.java
@@ -4,12 +4,39 @@ import org.avp.AliensVsPredator;
 import org.avp.client.model.entities.living.ModelDeacon;
 import org.avp.entities.living.EntityDeacon;
 
-import net.minecraft.client.renderer.entity.RenderManager;
+import com.asx.mdx.lib.client.util.OpenGL;
+import com.asx.mdx.lib.client.util.entity.RenderLivingWrapper;
 
-public class RenderDeacon extends RenderXenomorph<EntityDeacon, ModelDeacon>
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderDeacon extends RenderLivingWrapper<EntityDeacon, ModelDeacon>
 {
+    private float scale;
+
     public RenderDeacon(RenderManager m)
     {
-        super(m, AliensVsPredator.resources().models().DEACON, 0.75F);
+        super(m, AliensVsPredator.resources().models().DEACON);
+        this.scale = 1.0F;
+    }
+
+    @Override
+    protected void preRenderCallback(EntityDeacon xeno, float renderPartialTicks)
+    {
+        this.scale = 0.65F;
+        OpenGL.scale(this.scale, this.scale, this.scale);
+        super.preRenderCallback(xeno, renderPartialTicks);
+    }
+
+    @Override
+    public ResourceLocation getEntityTexture(EntityDeacon entity)
+    {
+        return this.getModel().getTexture();
+    }
+
+    public RenderDeacon setScale(float renderScale)
+    {
+        this.scale = renderScale;
+        return this;
     }
 }
