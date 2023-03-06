004dc4156d2ef79177eb438c1d50d74cf3eec610
bvanseg <bvanseg@gmail.com>
2023-03-06 16:08:12 -0500

    Use new brain task adapter type for the xenomorph brain.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/avp/util/brain/impl/XenomorphBrain.java
+++ src/main/java/org/avp/util/brain/impl/XenomorphBrain.java
@@ -1,17 +1,25 @@
 package org.avp.util.brain.impl;
 
+import org.avp.entities.ai.EntityAICustomAttackOnCollide;
+import org.avp.entities.ai.alien.EntityAIFindJelly;
+import org.avp.entities.ai.alien.EntityAIShareJelly;
 import org.avp.entities.ai.alien.EntitySelectorXenomorph;
+import org.avp.entities.living.species.SpeciesXenomorph;
 import org.avp.util.brain.AbstractBrain;
 import org.avp.util.brain.impl.sensor.EntityBrainSensor;
 import org.avp.util.brain.impl.sensor.NearestAttackableTargetBrainSensor;
-import org.avp.util.brain.impl.task.AttackOnCollideBrainTask;
-import org.avp.util.brain.impl.task.FindJellyBrainTask;
 import org.avp.util.brain.impl.task.NearestAttackableTargetBrainTask;
-import org.avp.util.brain.impl.task.WanderBrainTask;
-import org.avp.util.brain.impl.task.WatchClosestBrainTask;
+import org.avp.util.brain.task.BrainTaskAdapter;
 
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.player.EntityPlayer;
 
 /**
  * 
@@ -26,15 +34,30 @@ public class XenomorphBrain extends AbstractBrain<EntityBrainContext> {
 
 	@Override
 	public void init() {
-		// Senses
-		this.addSense(new EntityBrainSensor(10));
-		this.addSense(new NearestAttackableTargetBrainSensor(4, EntitySelectorXenomorph.instance));
+		// Brain Senses
+		this.addSense(new EntityBrainSensor(1));
+		this.addSense(new NearestAttackableTargetBrainSensor(1, EntitySelectorXenomorph.instance));
 		
-		// Tasks
-		this.addTask(new WanderBrainTask(0.8D));
-		this.addTask(new FindJellyBrainTask());
-		this.addTask(new WatchClosestBrainTask(EntityLivingBase.class, 16F));
+		// Brain Tasks
+//		this.addTask(new WanderBrainTask(0.8D));
+//		this.addTask(new FindJellyBrainTask());
+//		this.addTask(new WatchClosestBrainTask(EntityLivingBase.class, 16F));
 		this.addTask(new NearestAttackableTargetBrainTask());
-		this.addTask(new AttackOnCollideBrainTask(1.0D));
+//		this.addTask(new AttackOnCollideBrainTask(1.0D));
+
+		SpeciesXenomorph entity = (SpeciesXenomorph) this.getEntity();
+		
+		// NOTE: These are adapters for old minecraft AI.
+        this.addTask(new BrainTaskAdapter(new EntityAISwimming(entity)));
+        this.addTask(new BrainTaskAdapter(new EntityAILeapAtTarget(entity, 0.6F)));
+        this.addTask(new BrainTaskAdapter(new EntityAIWander(entity, 0.8D)));
+        this.addTask(new BrainTaskAdapter(new EntityAIFindJelly(entity)));
+        this.addTask(new BrainTaskAdapter(new EntityAIShareJelly(entity)));
+        this.addTask(new BrainTaskAdapter(new EntityAIWatchClosest(entity, EntityLivingBase.class, 16F)));
+        this.addTask(new BrainTaskAdapter(new EntityAICustomAttackOnCollide(entity, EntityLiving.class, 1.0D, false)));
+        this.addTask(new BrainTaskAdapter(new EntityAICustomAttackOnCollide(entity, EntityPlayer.class, 1.0D, false)));
+        this.addTask(new BrainTaskAdapter(new EntityAIHurtByTarget(entity, true)));
+        this.addTask(new BrainTaskAdapter(new EntityAINearestAttackableTarget<EntityLiving>(entity, EntityLiving.class, 0, false, false, EntitySelectorXenomorph.instance)));
+        this.addTask(new BrainTaskAdapter(new EntityAINearestAttackableTarget<EntityPlayer>(entity, EntityPlayer.class, 0, false, false, EntitySelectorXenomorph.instance)));
 	}
 }
