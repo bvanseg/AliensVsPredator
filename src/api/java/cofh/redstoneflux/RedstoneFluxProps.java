1119e08346739622c099b517def54cff31ff7675
bvanseg <bvanseg@gmail.com>
2023-03-12 01:58:24 -0500

    This shouldn't have been affected, blame eclipse.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/api/java/cofh/redstoneflux/RedstoneFluxProps.java
+++ src/api/java/cofh/redstoneflux/RedstoneFluxProps.java
@@ -6,7 +6,7 @@ public class RedstoneFluxProps {
 
 	}
 
-	public static final String MOD_ID = "cofh/redstoneflux";
+	public static final String MOD_ID = "redstoneflux";
 	public static final String MOD_NAME = "Redstone Flux";
 
 	public static final String VERSION = "2.1.0";
