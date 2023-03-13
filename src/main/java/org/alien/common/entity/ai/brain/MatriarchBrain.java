c466f8b74223d329047af8782edb95abdf72b85d
bvanseg <bvanseg@gmail.com>
2023-03-13 01:17:11 -0400

    Added MatriarchBrain.java.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

---
+++ src/main/java/org/alien/common/entity/ai/brain/MatriarchBrain.java
@@ -0,0 +1,26 @@
+package org.alien.common.entity.ai.brain;
+
+import org.alien.common.entity.ai.brain.task.MoveToHiveCoreBrainTask;
+import org.alien.common.entity.living.SpeciesXenomorph;
+
+/**
+ * 
+ * @author Boston Vanseghi
+ *
+ */
+public class MatriarchBrain extends XenomorphBrain {
+	public MatriarchBrain(SpeciesXenomorph entity) {
+		super(entity);
+	}
+
+	@Override
+	public void init() {
+		super.init();
+
+		// Brain Senses
+
+
+		// Brain Tasks
+		this.addTask(new MoveToHiveCoreBrainTask());
+	}
+}
