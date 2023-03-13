1332fe96730c4dcd5137d001d462039adfee313f
bvanseg <bvanseg@gmail.com>
2023-03-13 15:32:24 -0400

    Fixed Aethon attacking creative mode players.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/entity/ai/selector/EntitySelectorAethon.java
+++ src/main/java/org/alien/common/entity/ai/selector/EntitySelectorAethon.java
@@ -2,6 +2,7 @@ package org.alien.common.entity.ai.selector;
 
 import com.google.common.base.Predicate;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import org.alien.common.entity.living.EntityAethon;
 
 public class EntitySelectorAethon implements Predicate<EntityLivingBase>
@@ -12,9 +13,9 @@ public class EntitySelectorAethon implements Predicate<EntityLivingBase>
     public boolean apply(EntityLivingBase target)
     {
         if (target instanceof EntityAethon)
-        {
             return false;
-        }
+        if (target instanceof EntityPlayer && ((EntityPlayer)target).isCreative())
+            return false;
 
         return true;
     }
