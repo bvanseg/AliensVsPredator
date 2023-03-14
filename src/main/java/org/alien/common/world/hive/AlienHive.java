2a3821f93a41bc7048041a71c461ccc5237e9f39
bvanseg <bvanseg@gmail.com>
2023-03-13 20:43:07 -0400

    Cleaned up AlienHive code.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/world/hive/AlienHive.java
+++ src/main/java/org/alien/common/world/hive/AlienHive.java
@@ -1,6 +1,5 @@
 package org.alien.common.world.hive;
 
-import com.asx.mdx.lib.world.Pos;
 import com.google.common.primitives.Bytes;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
@@ -9,6 +8,7 @@ import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.util.Constants.NBT;
 import org.alien.common.entity.living.xenomorph.EntityMatriarch;
+import org.avp.AVP;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -41,7 +41,6 @@ public class AlienHive {
 		// If the matriarch is dead, the hive is dead. Update hives state.
 		if (this.matriarch.isDead) {
 			this.destroy();
-			return;
 		}
 	}
 	
@@ -72,10 +71,9 @@ public class AlienHive {
         return this.matriarch.getDistance(this.coreBlockPos.getX(), this.coreBlockPos.getY(), this.coreBlockPos.getZ()) < this.getCoreRange();
     }
 
-    // TODO: Use blockPos instead.
-    public boolean isPointWithinHive(Pos coord)
+    public boolean isPointWithinHive(BlockPos coord)
     {
-        return isPointWithinHive((int) coord.x(), (int) coord.y(), (int) coord.z());
+        return isPointWithinHive(coord.getX(), coord.getY(), coord.getZ());
     }
 
     public boolean isPointWithinHive(int x, int y, int z)
@@ -103,22 +101,22 @@ public class AlienHive {
         return 32;
     }
 	
-	private static final String matriarchIdNbtKey = "MatriarchId";
-	private static final String coreBlockPosNbtKey = "CoreBlockPos";
-	private static final String hiveMembersNbtKey = "HiveMembers";
-	private static final String resinPositionsNbtKey = "ResinPositions";
+	private static final String MATRIARCH_ID_NBT_KEY = "MatriarchId";
+	private static final String CORE_BLOCK_POS_NBT_KEY = "CoreBlockPos";
+	private static final String HIVE_MEMBERS_NBT_KEY = "HiveMembers";
+	private static final String RESIN_POSITIONS_NBT_KEY = "ResinPositions";
 
 	public void writeToNBT(NBTTagCompound nbt) {
 		// Write matriarch UUID.
-		nbt.setUniqueId(matriarchIdNbtKey, this.matriarch.getHiveMemberID());
+		nbt.setUniqueId(MATRIARCH_ID_NBT_KEY, this.matriarch.getHiveMemberID());
 
 		// Write core block position.
-		nbt.setIntArray(coreBlockPosNbtKey, new int[] { this.coreBlockPos.getX(), this.coreBlockPos.getY(), this.coreBlockPos.getZ() });
+		nbt.setIntArray(CORE_BLOCK_POS_NBT_KEY, new int[] { this.coreBlockPos.getX(), this.coreBlockPos.getY(), this.coreBlockPos.getZ() });
 		
 		// Write hive members.
 		NBTTagList memberList = new NBTTagList();
 		this.hiveMemberUUIDs.forEach(uuid -> new NBTTagString(uuid.toString()));
-		nbt.setTag(hiveMembersNbtKey, memberList);
+		nbt.setTag(HIVE_MEMBERS_NBT_KEY, memberList);
 		
 		// Write relative resin block positions.
 		ArrayList<Byte> resinPositions = new ArrayList<>();
@@ -127,29 +125,29 @@ public class AlienHive {
 			resinPositions.add((byte)(this.coreBlockPos.getY() - resinPos.getY()));
 			resinPositions.add((byte)(this.coreBlockPos.getZ() - resinPos.getZ()));
 		});
-		nbt.setByteArray(resinPositionsNbtKey, Bytes.toArray(resinPositions));
+		nbt.setByteArray(RESIN_POSITIONS_NBT_KEY, Bytes.toArray(resinPositions));
 	}
 
 	public void readFromNBT(NBTTagCompound nbt) {
 		// Sanity check.
-		UUID matriarchId = nbt.getUniqueId(matriarchIdNbtKey);
+		UUID matriarchId = nbt.getUniqueId(MATRIARCH_ID_NBT_KEY);
 		
 		if (matriarchId == null || matriarchId != this.matriarch.getHiveMemberID()) {
-			// TODO: Log warning here.
+			AVP.log().warn("Loaded an invalid matriarch id '{}', expected '{}'!", matriarchId, this.matriarch.getHiveMemberID());
 		}
 		
 		// Read core block position.
-		int[] coreBlockPos = nbt.getIntArray(coreBlockPosNbtKey);
-		if (coreBlockPos != null) {
-			this.coreBlockPos = new BlockPos(coreBlockPos[0], coreBlockPos[1], coreBlockPos[2]);
+		int[] coreBlockPosParts = nbt.getIntArray(CORE_BLOCK_POS_NBT_KEY);
+		if (coreBlockPosParts != null && coreBlockPosParts.length == 3) {
+			this.coreBlockPos = new BlockPos(coreBlockPosParts[0], coreBlockPosParts[1], coreBlockPosParts[2]);
 		}
 		
 		// Read hive members. Don't validate entity here, since queen can potentially load before other hive members.
-		NBTTagList memberUUIDStrings = nbt.getTagList(hiveMembersNbtKey, NBT.TAG_STRING);
+		NBTTagList memberUUIDStrings = nbt.getTagList(HIVE_MEMBERS_NBT_KEY, NBT.TAG_STRING);
 		memberUUIDStrings.forEach(uuidStringNBT -> this.hiveMemberUUIDs.add(UUID.fromString(uuidStringNBT.toString())));
 		
 		// Read relative resin block positions.
-		byte[] resinPositions = nbt.getByteArray(resinPositionsNbtKey);
+		byte[] resinPositions = nbt.getByteArray(RESIN_POSITIONS_NBT_KEY);
 		
 		for (int i = 0; i < resinPositions.length; i += 3) {
 			int posX = resinPositions[i];
