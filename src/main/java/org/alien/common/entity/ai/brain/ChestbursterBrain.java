1f98115bfb856c26bf10763b1256d0988f1b059b
bvanseg <bvanseg@gmail.com>
2023-03-14 18:07:08 -0400

    Added new avoid AI to chestburster brain.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/entity/ai/brain/ChestbursterBrain.java
+++ src/main/java/org/alien/common/entity/ai/brain/ChestbursterBrain.java
@@ -1,16 +1,11 @@
 package org.alien.common.entity.ai.brain;
 
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.ai.EntityAIAttackMelee;
-import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import org.alien.common.entity.ai.selector.EntitySelectorAvoid;
-import org.alien.common.entity.ai.selector.EntitySelectorParasitoid;
 import org.alien.common.entity.living.xenomorph.EntityChestburster;
 import org.lib.brain.impl.AbstractEntityBrain;
 import org.lib.brain.impl.sensor.EntityBrainSensor;
-import org.lib.brain.impl.sensor.NearestAttackableTargetBrainSensor;
+import org.lib.brain.impl.sensor.NearestAvoidTargetBrainSensor;
 import org.lib.brain.impl.task.*;
-import org.lib.brain.task.BrainTaskAdapter;
 
 /**
  * 
@@ -26,19 +21,21 @@ public class ChestbursterBrain extends AbstractEntityBrain<EntityChestburster> {
 	public void init() {
 		// Brain Senses
 		this.addSense(new EntityBrainSensor(1));
-		this.addSense(new NearestAttackableTargetBrainSensor(1, EntitySelectorParasitoid.instance));
+		// TODO:
+//		this.addSense(new NearestAttackableTargetBrainSensor(1, EntitySelectorParasitoid.instance));
+		this.addSense(new NearestAvoidTargetBrainSensor(1, EntitySelectorAvoid.instance));
 
 		// Brain Tasks
 		EntityChestburster entity = this.getEntity();
 		this.addTask(new SwimBrainTask(entity));
-		// TODO:
-		this.addTask(new BrainTaskAdapter(new EntityAIAvoidEntity<>(entity, EntityLivingBase.class, EntitySelectorAvoid.instance, 8.0F, 0.4F, 0.7F)));
+		this.addTask(new AvoidNearestAvoidTargetBrainTask(8.0F, 0.4F, 0.7F));
 		this.addTask(new AttackOnCollideBrainTask(0.800000011920929D));
 		this.addTask(new WanderBrainTask(0.800000011920929D));
 		this.addTask(new HurtByTargetBrainTask());
-		this.addTask(new NearestAttackableTargetBrainTask());
 		// TODO:
-		this.addTask(new BrainTaskAdapter(new EntityAIAttackMelee(entity, 0.8F, false)));
+//		this.addTask(new NearestAttackableTargetBrainTask());
+		// TODO:
+//		this.addTask(new BrainTaskAdapter(new EntityAIAttackMelee(entity, 0.8F, false)));
 		this.addTask(new LeapAtTargetBrainTask(0.8F));
 	}
 }
