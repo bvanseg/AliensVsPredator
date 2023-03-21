2ed5186242839acceebfd21893ac5fa6dfb87e67
bvanseg <bvanseg@gmail.com>
2023-03-21 10:26:57 -0400

    Queen now deals knockback on attack.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/entity/living/xenomorph/EntityMatriarch.java
+++ src/main/java/org/alien/common/entity/living/xenomorph/EntityMatriarch.java
@@ -1,6 +1,7 @@
 package org.alien.common.entity.living.xenomorph;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.item.ItemStack;
@@ -10,6 +11,7 @@ import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants.NBT;
@@ -101,6 +103,17 @@ public class EntityMatriarch extends SpeciesXenomorph implements IMob, HiveOwner
         if (this.world.getTotalWorldTime() % 20 == 0 && this.getHealth() > this.getMaxHealth() / 4) {
             this.heal(1F);
         }
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entity) {
+        boolean result = super.attackEntityAsMob(entity);
+
+        if (result && entity instanceof EntityLivingBase) {
+            ((EntityLivingBase)entity).knockBack(this, 2F, MathHelper.sin(this.rotationYaw * 0.017453292F), -MathHelper.cos(this.rotationYaw * 0.017453292F));
+        }
+
+        return result;
     }
 
     public boolean isReproducing() {
