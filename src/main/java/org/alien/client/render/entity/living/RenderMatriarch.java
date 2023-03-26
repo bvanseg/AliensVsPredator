1e1e6dde006a2a2175c5a66d0d6b50ed3200d74d
bvanseg <bvanseg@gmail.com>
2023-03-26 10:42:58 -0400

    Moved max psuedo jelly level for queen to AlienGrowthUtil.java.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/client/render/entity/living/RenderMatriarch.java
+++ src/main/java/org/alien/client/render/entity/living/RenderMatriarch.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.math.MathHelper;
 import org.alien.client.AlienResources;
 import org.alien.client.model.entity.living.ModelMatriarch;
+import org.alien.client.render.util.AlienGrowthUtil;
 import org.alien.common.entity.living.xenomorph.EntityMatriarch;
 
 public class RenderMatriarch extends RenderLivingWrapper<EntityMatriarch, ModelMatriarch>
@@ -15,8 +16,6 @@ public class RenderMatriarch extends RenderLivingWrapper<EntityMatriarch, ModelM
         super(m, AlienResources.instance.models().MATRIARCH);
     }
 
-    private static final int MAX_PSEUDO_JELLY_LEVEL = 2000;
-
     @Override
     protected void preRenderCallback(EntityMatriarch matriarch, float partialTicks)
     {
@@ -24,7 +23,7 @@ public class RenderMatriarch extends RenderLivingWrapper<EntityMatriarch, ModelM
         matriarch.growthProgress = matriarch.getJellyLevel();
         matriarch.growthProgress = jellyLevelPrev + ((matriarch.growthProgress - jellyLevelPrev) * partialTicks * 0.005f);
 
-        float additionalScale = MathHelper.clamp(matriarch.growthProgress / (MAX_PSEUDO_JELLY_LEVEL * 2), 0F, 1F);
+        float additionalScale = MathHelper.clamp(matriarch.growthProgress / (AlienGrowthUtil.MATRIARCH_MAX_PSEUDO_JELLY_LEVEL * 2), 0F, 1F);
 
         OpenGL.scale(1.75F + additionalScale, 1.75F + additionalScale, 1.75F + additionalScale);
     }
