dc9bc796998616523cba36f121c0e7d493ed7b95
bvanseg <bvanseg@gmail.com>
2023-04-02 07:42:58 -0400

    Use block properties in custom stairs.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/block/init/AlienStairsBlocks.java
+++ src/main/java/org/alien/common/block/init/AlienStairsBlocks.java
@@ -13,10 +13,10 @@ import org.lib.common.registry.BlockRegistryUtil;
 public class AlienStairsBlocks implements IPreInitEvent {
     public static final AlienStairsBlocks instance = new AlienStairsBlocks();
 
-    public static final Block UNIDENTIFIED_WOOD_STAIRS = new BlockCustomStairs(AlienVardaBlocks.UNIDENTIFIED_PLANKS.getDefaultState()).setHardness(5F).setResistance(15.0F).setLightOpacity(4).setRegistryName("unidentified_wood_stairs");
-    public static final Block PARADISE_SMALL_LOG_WOOD_STAIRS = new BlockCustomStairs(AlienParadiseBlocks.PARADISE_SMALL_LOG_PLANKS.getDefaultState()).setHardness(5F).setResistance(15.0F).setLightOpacity(4).setRegistryName("paradise_small_log_wood_stairs");
-    public static final Block PARADISE_MEDIUM_LOG_WOOD_STAIRS = new BlockCustomStairs(AlienParadiseBlocks.PARADISE_MEDIUM_LOG_PLANKS.getDefaultState()).setHardness(5F).setResistance(15.0F).setLightOpacity(4).setRegistryName("paradise_medium_log_wood_stairs");
-    public static final Block PARADISE_LARGE_LOG_WOOD_STAIRS = new BlockCustomStairs(AlienParadiseBlocks.PARADISE_LARGE_LOG_PLANKS.getDefaultState()).setHardness(5F).setResistance(15.0F).setLightOpacity(4).setRegistryName("paradise_large_log_wood_stairs");
+    public static final Block UNIDENTIFIED_WOOD_STAIRS = new BlockCustomStairs("unidentified_wood_stairs", AlienBlockProperties.WOOD_PLANK_PROPS, AlienVardaBlocks.UNIDENTIFIED_PLANKS.getDefaultState()).setLightOpacity(4);
+    public static final Block PARADISE_SMALL_LOG_WOOD_STAIRS = new BlockCustomStairs("paradise_small_log_wood_stairs", AlienBlockProperties.WOOD_PLANK_PROPS, AlienParadiseBlocks.PARADISE_SMALL_LOG_PLANKS.getDefaultState()).setLightOpacity(4);
+    public static final Block PARADISE_MEDIUM_LOG_WOOD_STAIRS = new BlockCustomStairs("paradise_medium_log_wood_stairs", AlienBlockProperties.WOOD_PLANK_PROPS, AlienParadiseBlocks.PARADISE_MEDIUM_LOG_PLANKS.getDefaultState()).setLightOpacity(4);
+    public static final Block PARADISE_LARGE_LOG_WOOD_STAIRS = new BlockCustomStairs("paradise_large_log_wood_stairs", AlienBlockProperties.WOOD_PLANK_PROPS, AlienParadiseBlocks.PARADISE_LARGE_LOG_PLANKS.getDefaultState()).setLightOpacity(4);
 
     @Override
     public void pre(FMLPreInitializationEvent event) {
