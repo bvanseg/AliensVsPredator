634b393796bf1aa3e1740d1d10e3fd170ac63aa7
bvanseg <bvanseg@gmail.com>
2023-04-02 06:24:47 -0400

    Do not create new array every time while generating surface.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/world/worldgen/WorldGenerator.java
+++ src/main/java/org/alien/common/world/worldgen/WorldGenerator.java
@@ -27,6 +27,37 @@ public class WorldGenerator implements IWorldGenerator
     private static final WorldGenMinable WORLDGEN_ORE_LITHIUM = new WorldGenMinable(AVPOreBlocks.ORE_LITHIUM.getDefaultState(), 12);
     private static final WorldGenMinable WORLDGEN_ORE_COBALT = new WorldGenMinable(AVPOreBlocks.ORE_COBALT.getDefaultState(), 6);
 
+    private static final Biome[] VALID_ORE_BIOMES = new Biome[]{
+            Biomes.BEACH,
+            Biomes.BIRCH_FOREST,
+            Biomes.BIRCH_FOREST_HILLS,
+            Biomes.COLD_BEACH,
+            Biomes.COLD_TAIGA,
+            Biomes.COLD_TAIGA_HILLS,
+            Biomes.DESERT,
+            Biomes.DESERT_HILLS,
+            Biomes.EXTREME_HILLS,
+            Biomes.EXTREME_HILLS_EDGE,
+            Biomes.EXTREME_HILLS_WITH_TREES,
+            Biomes.FOREST,
+            Biomes.FOREST_HILLS,
+            Biomes.FROZEN_OCEAN,
+            Biomes.FROZEN_RIVER,
+            Biomes.ICE_MOUNTAINS,
+            Biomes.ICE_PLAINS,
+            Biomes.JUNGLE,
+            Biomes.JUNGLE_EDGE,
+            Biomes.JUNGLE_HILLS,
+            Biomes.PLAINS,
+            Biomes.RIVER,
+            Biomes.ROOFED_FOREST,
+            Biomes.SWAMPLAND,
+            Biomes.TAIGA,
+            Biomes.TAIGA_HILLS,
+            Biomes.OCEAN,
+            Biomes.DEEP_OCEAN
+    };
+
     @Override
     public void generate(Random seed, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
     {
@@ -36,23 +67,21 @@ public class WorldGenerator implements IWorldGenerator
     private void generateSurface(World world, Random rand, int chunkX, int chunkZ)
     {
         BlockPos chunkCoords = new BlockPos(chunkX, 0, chunkZ);
-        Biome biome = world.getBiome(new BlockPos(chunkX, 0, chunkZ));
-        Biome[] overworldBiomes = new Biome[] { Biomes.BEACH, Biomes.BIRCH_FOREST, Biomes.BIRCH_FOREST_HILLS, Biomes.COLD_BEACH, Biomes.COLD_TAIGA, Biomes.COLD_TAIGA_HILLS, Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.EXTREME_HILLS, Biomes.EXTREME_HILLS_EDGE, Biomes.EXTREME_HILLS_WITH_TREES, Biomes.FOREST, Biomes.FOREST_HILLS, Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER, Biomes.ICE_MOUNTAINS, Biomes.ICE_PLAINS, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.PLAINS, Biomes.RIVER, Biomes.ROOFED_FOREST, Biomes.SWAMPLAND, Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.OCEAN, Biomes.DEEP_OCEAN
-        };
+        Biome biome = world.getBiome(chunkCoords);
         
-        Worlds.generateInBiome(world, WORLDGEN_ORE_BAUXITE, rand, 20, 16, 128, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, WORLDGEN_ORE_COPPER, rand, 20, 0, 128, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, WORLDGEN_ORE_SILICON, rand, 4, 1, 64, chunkCoords, overworldBiomes);
-        Worlds.generateInBiome(world, WORLDGEN_ORE_MONAZITE, rand, 4, 1, 48, chunkCoords, overworldBiomes);
+        Worlds.generateInBiome(world, WORLDGEN_ORE_BAUXITE, rand, 20, 16, 128, chunkCoords, VALID_ORE_BIOMES);
+        Worlds.generateInBiome(world, WORLDGEN_ORE_COPPER, rand, 20, 0, 128, chunkCoords, VALID_ORE_BIOMES);
+        Worlds.generateInBiome(world, WORLDGEN_ORE_SILICON, rand, 4, 1, 64, chunkCoords, VALID_ORE_BIOMES);
+        Worlds.generateInBiome(world, WORLDGEN_ORE_MONAZITE, rand, 4, 1, 48, chunkCoords, VALID_ORE_BIOMES);
         
         if (biome == Biomes.BEACH || biome == Biomes.OCEAN || biome == Biomes.DEEP_OCEAN || biome == Biomes.FROZEN_OCEAN || biome == Biomes.FROZEN_RIVER)
         {
-            Worlds.generateInBiome(world, WORLDGEN_ORE_LITHIUM, rand, 1, 1, 48, chunkCoords, overworldBiomes);
+            Worlds.generateInBiome(world, WORLDGEN_ORE_LITHIUM, rand, 1, 1, 48, chunkCoords, VALID_ORE_BIOMES);
         }
         
         if (biome == Biomes.JUNGLE || biome == Biomes.JUNGLE_EDGE || biome == Biomes.JUNGLE_HILLS || biome == Biomes.MUTATED_JUNGLE || biome == Biomes.MUTATED_JUNGLE_EDGE)
         {
-            Worlds.generateInBiome(world, WORLDGEN_ORE_COBALT, rand, 1, 1, 64, chunkCoords, overworldBiomes);
+            Worlds.generateInBiome(world, WORLDGEN_ORE_COBALT, rand, 1, 1, 64, chunkCoords, VALID_ORE_BIOMES);
         }
     }
 }
