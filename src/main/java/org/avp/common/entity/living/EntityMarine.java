3726838942a0fe43add59a5b0f9b5a94a7f3883f
bvanseg <bvanseg@gmail.com>
2023-04-03 22:59:45 -0400

    Added MarineDecorator.java.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/avp/common/entity/living/EntityMarine.java
+++ src/main/java/org/avp/common/entity/living/EntityMarine.java
@@ -21,11 +21,8 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.util.Constants;
 import org.avp.client.AVPSounds;
 import org.avp.common.AVPItems;
@@ -36,24 +33,10 @@ import org.lib.brain.Brainiac;
 import org.lib.brain.impl.BrainMemoryKeys;
 import org.lib.common.inventory.InventorySnapshot;
 
-import java.util.Set;
 import java.util.UUID;
 
 public class EntityMarine extends EntityCreature implements IMob, IRangedAttackMob, Brainiac<MarineBrain>
 {
-    protected static final int[] SKIN_TONES = new int[] { 0xF1C381FF, 0xC58C85FF, 0xECBCB4FF, 0xD1A3A4FF, 0xA1665EFF, 0x503335FF, 0x592F2AFF };
-    protected static final int[] EYE_COLORS = new int[] {
-            0x4D1500FF, // Classic brownish-black
-            0x8BB5DBFF, // Light Blue
-            0x4079C0FF, // Blue
-            0xB3B9B9FF, // Light Gray
-            0x8E9796FF, // Gray
-            0x748B9DFF, // Blue-Gray
-            0x6E9D4CFF, // Green
-            0xAA6925FF, // Amber
-            0x863603FF, // Light Brown
-            0x3f0c08FF  // Dark Brown
-    };
 
     private static final DataParameter<Boolean> AIMING = EntityDataManager.createKey(EntityMarine.class, DataSerializers.BOOLEAN);
     private static final DataParameter<Integer> TYPE   = EntityDataManager.createKey(EntityMarine.class, DataSerializers.VARINT);
@@ -109,58 +92,11 @@ public class EntityMarine extends EntityCreature implements IMob, IRangedAttackM
         super.entityInit();
         this.getDataManager().register(AIMING, false);
 
-        this.getDataManager().register(TYPE, this.world.rand.nextInt(MarineTypes.values().length));
-        this.getDataManager().register(SKIN_TONE, SKIN_TONES[this.world.rand.nextInt(SKIN_TONES.length)]);
+        this.getDataManager().register(TYPE, MarineDecorator.generateRandomWeaponType(this));
+        this.getDataManager().register(SKIN_TONE, MarineDecorator.generateRandomSkinTone(this));
         this.getDataManager().register(CAMO_COLOR, -1);
-        this.getDataManager().register(EYE_COLOR, EYE_COLORS[this.world.rand.nextInt(EYE_COLORS.length)]);
+        this.getDataManager().register(EYE_COLOR, MarineDecorator.generateRandomEyeColor(this));
         this.getDataManager().register(SQUAD_LEADER_UNIQUE_ID, Optional.absent());
-    }
-
-    private int generateCamoColorBasedOnCurrentBiome() {
-        Biome biome = this.world.getBiome(this.getPosition());
-
-        int color;
-
-        Set<BiomeDictionary.Type> biomeTypes = BiomeDictionary.getTypes(biome);
-
-        if (biomeTypes.contains(BiomeDictionary.Type.NETHER)) {
-            color = 0x85_42_42_FF; // Netherrack
-        } else if (biomeTypes.contains(BiomeDictionary.Type.END)) {
-            color = 0xEE_F6_B4_FF; // End Stone
-        } else if (biomeTypes.contains(BiomeDictionary.Type.MUSHROOM)) {
-            color = 0x8B_71_73_FF; // Mycelium
-        } else if (!world.canBlockSeeSky(this.getPosition())) {
-            color = 0x74_74_74_FF; // Underground
-        }
-        else if (biomeTypes.contains(BiomeDictionary.Type.SNOWY)) {
-            color = 0xFA_FA_FA_FF; // Snow
-        } else if (biomeTypes.contains(BiomeDictionary.Type.SANDY) || biomeTypes.contains(BiomeDictionary.Type.BEACH)) {
-            color = 0xD5_C4_96_FF; // Sand
-        } else if (biomeTypes.contains(BiomeDictionary.Type.OCEAN)) {
-            color = 0x1F_3C_5A_FF; // Water
-        } else if (biomeTypes.contains(BiomeDictionary.Type.RIVER)) {
-            color = 0x30_5A_9C_FF; // Water (river)
-        } else if (biomeTypes.contains(BiomeDictionary.Type.SWAMP)) {
-            color = 0x2F_5C_41_FF; // Swamp Water
-        } else if (biomeTypes.contains(BiomeDictionary.Type.FOREST)) {
-            color = 0x3C_67_3C_FF; // Leaves (forest)
-        } else if (biomeTypes.contains(BiomeDictionary.Type.PLAINS)) {
-            color = 0x8B_AC_45_FF; // Grass
-        } else if (biomeTypes.contains(BiomeDictionary.Type.MOUNTAIN)) {
-            color = 0x7F_7F_7F_FF; // Stone
-        } else if (biomeTypes.contains(BiomeDictionary.Type.HILLS)) {
-            color = 0x8B_72_3E_FF; // Dirt
-        } else if (biomeTypes.contains(BiomeDictionary.Type.JUNGLE)) {
-            color = 0x50_76_35_FF; // Leaves (jungle)
-        } else if (biomeTypes.contains(BiomeDictionary.Type.SAVANNA)) {
-            color = 0xBF_A6_5A_FF; // Grass (savanna)
-        } else {
-            // Default coloring for camo is white.
-            color = 0xAA_AA_AA_FF;
-        }
-
-
-        return color;
     }
 
     @Override
@@ -170,12 +106,6 @@ public class EntityMarine extends EntityCreature implements IMob, IRangedAttackM
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.6499999761581421D);
         this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(10.0D);
-    }
-
-    @Override
-    public float getBlockPathWeight(BlockPos pos)
-    {
-        return 0.5F - this.world.getLightBrightness(pos);
     }
 
     @Override
@@ -207,7 +137,7 @@ public class EntityMarine extends EntityCreature implements IMob, IRangedAttackM
             // is being invoked, the entity is positioned at 0, 0, 0, and will always get the biome
             // at that position instead of its true spawn position. So we initialize the camo color here, instead.
             if (this.getCamoColor() == -1) {
-                this.getDataManager().set(CAMO_COLOR, this.generateCamoColorBasedOnCurrentBiome());
+                this.getDataManager().set(CAMO_COLOR, MarineDecorator.generateCamoColorBasedOnCurrentBiome(this));
             }
 
             this.brain.update();
