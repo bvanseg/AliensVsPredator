e1c2b682a88576c6445dcdc9e286f5d6c5054270
bvanseg <bvanseg@gmail.com>
2023-04-11 19:22:43 -0400

    Removed SpeciesAlien#frame, cleaned up SpeciesAlien.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/entity/living/SpeciesAlien.java
+++ src/main/java/org/alien/common/entity/living/SpeciesAlien.java
@@ -26,143 +26,123 @@ import org.avp.common.AVPDamageSources;
 
 import java.util.UUID;
 
-public abstract class SpeciesAlien extends EntityMob implements IMob, RoyalOrganism, IAnimated
-{
-    private static final DataParameter<Integer> JELLY_LEVEL       = EntityDataManager.createKey(SpeciesAlien.class, DataSerializers.VARINT);
-    private UUID                                signature;
-    protected boolean                           jellyLimitOverride;
-    protected boolean                           isDependant       = false;
+/**
+ * @author Ri5ux
+ */
+public abstract class SpeciesAlien extends EntityMob implements IMob, RoyalOrganism, IAnimated {
+    private static final DataParameter<Integer> JELLY_LEVEL = EntityDataManager.createKey(SpeciesAlien.class, DataSerializers.VARINT);
+    private UUID signature;
+    protected boolean jellyLimitOverride;
+    protected boolean isDependant = false;
 
-    /** Animations **/
-    public int                                  frame;
-    private int                                 animationTick;
-    protected Animation animation         =     NO_ANIMATION;
-    protected boolean                           isAnimationPaused = false;
+    /**
+     * Animations
+     **/
+    private int animationTick;
+    protected Animation animation = NO_ANIMATION;
+    protected boolean isAnimationPaused = false;
 
     public float growthProgress;
     public boolean growthInitialized = false;
 
-    protected SpeciesAlien(World world)
-    {
+    protected SpeciesAlien(World world) {
         super(world);
         this.jumpMovementFactor = 0.2F;
         this.jellyLimitOverride = false;
     }
 
     @Override
-    protected void entityInit()
-    {
+    protected void entityInit() {
         super.entityInit();
         this.getDataManager().register(JELLY_LEVEL, this.getJellyLevelStart());
     }
 
     @Override
-    public void writeEntityToNBT(NBTTagCompound nbt)
-    {
+    public void writeEntityToNBT(NBTTagCompound nbt) {
         super.writeEntityToNBT(nbt);
         this.writeAlienToNBT(nbt);
     }
 
-    public void writeAlienToNBT(NBTTagCompound nbt)
-    {
+    public void writeAlienToNBT(NBTTagCompound nbt) {
         nbt.setString("HiveSignature", signature != null ? this.signature.toString() : "");
         nbt.setInteger("JellyLevel", this.getJellyLevel());
     }
 
     @Override
-    public void readEntityFromNBT(NBTTagCompound nbt)
-    {
+    public void readEntityFromNBT(NBTTagCompound nbt) {
         super.readEntityFromNBT(nbt);
         this.readAlienFromNBT(nbt);
     }
 
-    public void readAlienFromNBT(NBTTagCompound nbt)
-    {
+    public void readAlienFromNBT(NBTTagCompound nbt) {
         this.signature = Worlds.uuidFromNBT(nbt, "HiveSignature");
         this.setJellyLevel(nbt.getInteger("JellyLevel"));
     }
 
     @Override
-    public void onKillEntity(EntityLivingBase entity)
-    {
+    public void onKillEntity(EntityLivingBase entity) {
         super.onKillEntity(entity);
         this.setJellyLevel(this.getJellyLevel() + 250);
     }
 
     @Override
-    protected boolean canDespawn()
-    {
+    protected boolean canDespawn() {
         return false;
     }
 
-    protected void spawnAcidPool()
-    {
+    protected void spawnAcidPool() {
         EntityAcidPool entity = new EntityAcidPool(this.world);
         entity.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, this.rotationPitch);
         this.world.spawnEntity(entity);
     }
-    
+
     @Override
-    public boolean attackEntityFrom(DamageSource source, float amount)
-    {
-        if(!this.world.isRemote && this.getHealth() > amount && source != DamageSource.IN_WALL && !source.isProjectile() && !source.isMagicDamage() && (this.getMaxHealth() * 0.25 <= amount || this.rand.nextInt(30) == 0))
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        if (!this.world.isRemote && this.getHealth() > amount && source != DamageSource.IN_WALL && !source.isProjectile() && !source.isMagicDamage() && (this.getMaxHealth() * 0.25 <= amount || this.rand.nextInt(30) == 0))
             this.spawnAcidPool();
         return super.attackEntityFrom(source, amount);
     }
 
     @Override
-    public void onDeath(DamageSource damagesource)
-    {
+    public void onDeath(DamageSource damagesource) {
         super.onDeath(damagesource);
 
-        if (!this.world.isRemote)
-        {
-            if (damagesource != DamageSource.ON_FIRE && damagesource != DamageSource.IN_FIRE && damagesource != AVPDamageSources.FLAMETHROWER)
-            {
+        if (!this.world.isRemote) {
+            if (damagesource != DamageSource.ON_FIRE && damagesource != DamageSource.IN_FIRE && damagesource != AVPDamageSources.FLAMETHROWER) {
                 this.spawnAcidPool();
             }
 
             int adjustedLevel = this.getJellyLevel() / 4;
 
-            if (!this.jellyLimitOverride)
-            {
+            if (!this.jellyLimitOverride) {
                 adjustedLevel = Math.min(adjustedLevel, 64);
             }
 
-            if (this.isDependant)
-            {
+            if (this.isDependant) {
                 new ItemDrop(100, new ItemStack(AlienItems.ITEM_ROYAL_JELLY, adjustedLevel)).tryDrop(this);
             }
         }
     }
 
-    protected void onPickupJelly(EntityItem entityItem)
-    {
+    protected void onPickupJelly(EntityItem entityItem) {
         this.setJellyLevel(this.getJellyLevel() + (entityItem.getItem().getCount() * 100));
         entityItem.setDead();
     }
 
-    protected void updateAnimations()
-    {
-        frame++;
-
-        if (getActiveAnimation() != NO_ANIMATION)
-        {
-            if (!this.isAnimationPaused())
-            {
+    protected void updateAnimations() {
+        if (getActiveAnimation() != NO_ANIMATION) {
+            if (!this.isAnimationPaused()) {
                 animationTick++;
             }
 
-            if (animationTick >= animation.getDuration())
-            {
+            if (animationTick >= animation.getDuration()) {
                 onAnimationFinish(animation);
             }
         }
     }
 
     @Override
-    public void onUpdate()
-    {
+    public void onUpdate() {
         super.onUpdate();
         this.updateAnimations();
     }
@@ -186,88 +166,75 @@ public abstract class SpeciesAlien extends EntityMob implements IMob, RoyalOrgan
     }
 
     @Override
-    public int getJellyLevel()
-    {
+    public int getJellyLevel() {
         return this.getDataManager().get(JELLY_LEVEL);
     }
 
     @Override
-    public void setJellyLevel(int level)
-    {
+    public void setJellyLevel(int level) {
         this.getDataManager().set(JELLY_LEVEL, level);
     }
 
-    protected void negateFallDamage()
-    {
+    protected void negateFallDamage() {
         this.fallDistance = 0F;
     }
 
-    protected int getJellyLevelStart()
-    {
+    protected int getJellyLevelStart() {
         return 0;
     }
 
     @Override
-    protected void despawnEntity()
-    {
+    protected void despawnEntity() {
     }
 
-    /** Animation Dependent **/
+    /**
+     * Animation Dependent
+     **/
 
     @Override
-    public int getAnimationTick()
-    {
+    public int getAnimationTick() {
         return this.animationTick;
     }
 
     @Override
-    public void setAnimationTick(int tick)
-    {
+    public void setAnimationTick(int tick) {
         this.animationTick = tick;
     }
 
     @Override
-    public Animation getActiveAnimation()
-    {
+    public Animation getActiveAnimation() {
         return this.animation;
     }
 
     @Override
-    public void setActiveAnimation(Animation animation)
-    {
+    public void setActiveAnimation(Animation animation) {
         this.animation = animation;
         setAnimationTick(0);
     }
 
     @Override
-    public Animation[] getAnimations()
-    {
-        return new Animation[] {};
+    public Animation[] getAnimations() {
+        return new Animation[]{};
     }
 
     @Override
-    public boolean isAnimationPaused()
-    {
+    public boolean isAnimationPaused() {
         return this.isAnimationPaused;
     }
 
     @Override
-    public void pauseAnimation()
-    {
+    public void pauseAnimation() {
         this.isAnimationPaused = true;
     }
 
     @Override
-    public void playAnimation()
-    {
+    public void playAnimation() {
         this.isAnimationPaused = false;
     }
 
-    protected void onAnimationFinish(Animation animation)
-    {
+    protected void onAnimationFinish(Animation animation) {
         /** If the set animation duration is 0, do not expire. **/
-        if (animation.getDuration() > 0)
-        {
+        if (animation.getDuration() > 0) {
             setActiveAnimation(NO_ANIMATION);
         }
     }
