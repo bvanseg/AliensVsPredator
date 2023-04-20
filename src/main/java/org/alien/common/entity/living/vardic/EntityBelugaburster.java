c6b50cf52bc4dff331357c28713fc69ae411adda
bvanseg <bvanseg@gmail.com>
2023-04-20 15:14:27 -0400

    Cleaned up belugaburster.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/entity/living/vardic/EntityBelugaburster.java
+++ src/main/java/org/alien/common/entity/living/vardic/EntityBelugaburster.java
@@ -1,6 +1,5 @@
 package org.alien.common.entity.living.vardic;
 
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
@@ -14,17 +13,6 @@ public class EntityBelugaburster extends EntityChestburster
     {
         super(world);
         this.setSize(0.25F, 0.7F);
-        this.experienceValue = 16;
-    }
-
-    @Override
-    protected void applyEntityAttributes()
-    {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(14.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.6499999761581421D);
-        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(0.5D);
-        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(32.0D);
     }
     
     @Override
