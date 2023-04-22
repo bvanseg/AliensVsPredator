c98c926758e394f40eaa4973a6d0bb527aeda7dd
bvanseg <bvanseg@gmail.com>
2023-04-22 17:24:39 -0400

    Added heartbeat sounds during impregnation stages.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/alien/common/world/EntityImpregnationHandler.java
+++ src/main/java/org/alien/common/world/EntityImpregnationHandler.java
@@ -7,12 +7,14 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import org.alien.client.AlienSounds;
 import org.alien.common.world.capability.Organism.Provider;
 import org.alien.common.world.capability.OrganismImpl;
 import org.lib.common.FuncUtil;
@@ -60,29 +62,44 @@ public class EntityImpregnationHandler
             double stageThreePercentage = 0.25;
             double stageFourPercentage = 0.1;
 
+            int currentStage = (int) MDXMath.map(age, gestationPeriod * (1 - stageOnePercentage), gestationPeriod * (1 - stageFourPercentage), 1, 4);
+
             // Stage 1, the player develops an increasing hunger up to hunger 100.
             if (ticksUntilBirth < gestationPeriod * stageOnePercentage) {
-                int hungerAmplifier = (int) MDXMath.map(age, gestationPeriod * (1 - stageOnePercentage), gestationPeriod, 1, 100);
-                host.addPotionEffect(new PotionEffect(MobEffects.HUNGER, ticksUntilBirth, hungerAmplifier));
+                int hungerAmplifier = (int) MDXMath.map(age, gestationPeriod * (1 - stageOnePercentage), gestationPeriod, 1, 40);
+                host.addPotionEffect(new PotionEffect(MobEffects.HUNGER, gestationPeriod, hungerAmplifier));
             }
 
             // Stage 2, the player becomes weakened as the embryo takes hold within them.
             if (ticksUntilBirth < gestationPeriod * stageTwoPercentage) {
-                host.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, ticksUntilBirth));
+                host.addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, gestationPeriod));
             }
 
             // Stage 3, the player is losing so much energy that slowness and mining fatigue take place.
             int stageThreeAmplifier = (int) MDXMath.map(age, gestationPeriod * (1 - stageThreePercentage), gestationPeriod, 1, 4);
             if (ticksUntilBirth < gestationPeriod * stageThreePercentage) {
-                host.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, ticksUntilBirth, stageThreeAmplifier));
-                host.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, ticksUntilBirth, stageThreeAmplifier));
+                host.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, gestationPeriod, stageThreeAmplifier));
+                host.addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, gestationPeriod, stageThreeAmplifier));
             }
 
             // Stage 4, the player's bodily organs are so heavily damaged that nausea and blindness set in. Death is imminent.
             int stageFourAmplifier = (int) MDXMath.map(age, gestationPeriod * (1 - stageFourPercentage), gestationPeriod, 1, 4);
             if (ticksUntilBirth < gestationPeriod * stageFourPercentage) {
-                host.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, ticksUntilBirth, stageFourAmplifier));
-                host.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, ticksUntilBirth, stageFourAmplifier));
+                host.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, gestationPeriod, stageFourAmplifier));
+                host.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, gestationPeriod, stageFourAmplifier));
+            }
+
+            if (currentStage == 1 && world.getTotalWorldTime() % 20 == 0) {
+                world.playSound(null, host.getPosition(), AlienSounds.HEARTBEAT_NORMAL.event(), SoundCategory.HOSTILE, 0.5F, 1F);
+            }
+            else if (currentStage == 2 && world.getTotalWorldTime() % 15 == 0) {
+                world.playSound(null, host.getPosition(), AlienSounds.HEARTBEAT_ELEVATED.event(), SoundCategory.HOSTILE, 1F, 1F);
+            }
+            else if (currentStage == 3 && world.getTotalWorldTime() % 10 == 0) {
+                world.playSound(null, host.getPosition(), AlienSounds.HEARTBEAT_RAPID.event(), SoundCategory.HOSTILE, 1.5F, 1F);
+            }
+            else if (currentStage >= 4 && world.getTotalWorldTime() % 8 == 0) {
+                world.playSound(null, host.getPosition(), AlienSounds.HEARTBEAT_FATAL.event(), SoundCategory.HOSTILE, 2F, 1F);
             }
 
             // Congratulations, it's the birth of a billion dollar franchise!
