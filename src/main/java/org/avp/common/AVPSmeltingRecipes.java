f27c8d362c22a2378c786e7a918ef6473b839104
bvanseg <bvanseg@gmail.com>
2023-04-23 11:22:43 -0400

    Fixed paradise logs not having a charcoal smelting recipe.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/avp/common/AVPSmeltingRecipes.java
+++ src/main/java/org/avp/common/AVPSmeltingRecipes.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.alien.common.AlienItems;
+import org.alien.common.block.init.AlienParadiseBlocks;
 import org.alien.common.block.init.AlienVardaBlocks;
 import org.avp.common.block.init.AVPOreBlocks;
 import org.avp.common.item.init.AVPItems;
@@ -35,6 +36,10 @@ public class AVPSmeltingRecipes
         GameRegistry.addSmelting(AVPOreBlocks.ORE_BAUXITE, new ItemStack(AVPItems.ITEM_INGOT_ALUMINUM), 1.0F);
         GameRegistry.addSmelting(AVPOreBlocks.ORE_SILICON, new ItemStack(AVPItems.ITEM_SILICON), 1.0F);
         GameRegistry.addSmelting(AlienItems.ITEM_RAW_TENTACLE, new ItemStack(AlienItems.ITEM_TRILO_BITE), 2.0F);
+
         GameRegistry.addSmelting(AlienVardaBlocks.GIGER_LOG, CHARCOAL, 1.0F);
+        GameRegistry.addSmelting(AlienParadiseBlocks.PARADISE_LOG_SMALL, CHARCOAL, 1.0F);
+        GameRegistry.addSmelting(AlienParadiseBlocks.PARADISE_LOG_MED, CHARCOAL, 1.0F);
+        GameRegistry.addSmelting(AlienParadiseBlocks.PARADISE_LOG_LARGE, CHARCOAL, 1.0F);
     }
 }
