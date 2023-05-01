ed7321c49a718fd671635b7f1f6e188228ffcafe
bvanseg <bvanseg@gmail.com>
2023-05-01 13:57:27 -0400

    Extracted yautja cloaking logic out to its own helper class.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

---
+++ src/main/java/org/predator/common/entity/living/helper/YautjaCloakHelper.java
@@ -0,0 +1,94 @@
+package org.predator.common.entity.living.helper;
+
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.math.MathHelper;
+import org.avp.common.network.AvpDataSerializers;
+import org.predator.client.PredatorSounds;
+import org.predator.common.entity.living.SpeciesYautja;
+import org.predator.common.entity.state.CloakState;
+
+/**
+ * @author Boston Vanseghi
+ */
+public class YautjaCloakHelper {
+
+    private YautjaCloakHelper() { /* Do Nothing */ }
+
+    public static final DataParameter<CloakState> CLOAK_STATE = EntityDataManager.createKey(SpeciesYautja.class, AvpDataSerializers.CLOAK_STATE);
+    private static final DataParameter<Integer> CLOAK_PROGRESS = EntityDataManager.createKey(SpeciesYautja.class, DataSerializers.VARINT);
+
+    public static final int MAX_CLOAK = 20 * 2;
+    public static final int MIN_CLOAK = 0;
+    public static final int CLOAK_PROGRESS_SPEED = 1;
+
+    public static void entityInit(SpeciesYautja yautja) {
+        yautja.getDataManager().register(CLOAK_STATE, CloakState.DECLOAKED);
+        yautja.getDataManager().register(CLOAK_PROGRESS, MIN_CLOAK);
+    }
+
+
+    public static void tickCloakingLogic(SpeciesYautja yautja) {
+        if (!yautja.world.isRemote) {
+            // Yautja should only force de-cloak in water, snow doesn't count.
+            boolean isInRain = yautja.world.isRainingAt(yautja.getPosition()) && !yautja.world.getBiome(yautja.getPosition()).getEnableSnow();
+            if (yautja.isInWater() || isInRain || yautja.getAttackTarget() != null) {
+                handleDecloak(yautja);
+            } else {
+                handleCloak(yautja);
+            }
+        }
+
+        updateCloakingProgress(yautja);
+    }
+
+    private static void updateCloakingProgress(SpeciesYautja yautja) {
+        switch (yautja.getCloakState()) {
+            case CLOAKED:
+                yautja.cloakProgress = MAX_CLOAK;
+                break;
+            case CLOAKING:
+                yautja.cloakProgress += CLOAK_PROGRESS_SPEED;
+                break;
+            case DECLOAKING_FORCED:
+                yautja.cloakProgress -= CLOAK_PROGRESS_SPEED;
+                break;
+            case DECLOAKING_MANUAL:
+                yautja.cloakProgress -= CLOAK_PROGRESS_SPEED * 3;
+                break;
+            default:
+                yautja.cloakProgress = MIN_CLOAK;
+                break;
+        }
+
+        yautja.cloakProgress = MathHelper.clamp(yautja.cloakProgress, MIN_CLOAK, MAX_CLOAK);
+    }
+
+    private static void handleCloak(SpeciesYautja yautja) {
+        if (yautja.cloakProgress < MAX_CLOAK) {
+            if (yautja.getCloakState() != CloakState.CLOAKING) {
+                PredatorSounds.YAUTJA_CLOAK.playSound(yautja, 0.6F, 1.0F);
+            }
+
+            yautja.setCloakState(CloakState.CLOAKING);
+        } else if (yautja.cloakProgress == MAX_CLOAK) {
+            yautja.setCloakState(CloakState.CLOAKED);
+        }
+    }
+
+    private static void handleDecloak(SpeciesYautja yautja) {
+        CloakState decloakType = yautja.getAttackTarget() != null ? CloakState.DECLOAKING_MANUAL : CloakState.DECLOAKING_FORCED;
+
+        if (yautja.cloakProgress > MIN_CLOAK) {
+            if (yautja.getCloakState() != CloakState.DECLOAKING_FORCED && yautja.getCloakState() != CloakState.DECLOAKING_MANUAL) {
+                PredatorSounds.YAUTJA_DECLOAK.playSound(yautja, 0.6F, 1.0F);
+            }
+
+            yautja.setCloakState(decloakType);
+        }
+        else if (yautja.cloakProgress == MIN_CLOAK) {
+            yautja.setCloakState(CloakState.DECLOAKED);
+        }
+    }
+}
