ed8ee11dec639bf53cd57e8615dbb9df5525f051
bvanseg <bvanseg@gmail.com>
2023-05-06 00:08:21 -0400

    Fixed avp entities forgetting their attack target if they can not see them.
    Signed-off-by: bvanseg <bvanseg@gmail.com>

--- src/main/java/org/lib/brain/impl/sensor/NearestAttackableTargetBrainSensor.java
+++ src/main/java/org/lib/brain/impl/sensor/NearestAttackableTargetBrainSensor.java
@@ -36,20 +36,40 @@ public class NearestAttackableTargetBrainSensor extends AbstractBrainSensor<Enti
 		
 		Optional<List<EntityLivingBase>> livingEntitiesOptional = ctx.getBrain().getMemory(BrainMemoryKeys.LIVING_ENTITIES);
 
-		if (livingEntitiesOptional.isPresent()) {
-			List<EntityLivingBase> livingEntities = livingEntitiesOptional.get();
-			List<EntityLivingBase> targets = livingEntities.stream().filter(
-				target -> !target.isDead &&
-						target.getHealth() > 0 &&
-						targetPredicate.test(target) &&
-						ctx.getEntity().canEntityBeSeen(target)
-			).collect(Collectors.toList());
+		Optional<Entity> nearestAttackableTargetOptional = ctx.getBrain().getMemory(BrainMemoryKeys.NEAREST_ATTACKABLE_TARGET);
 
-			if (!targets.isEmpty()) {
-		        ctx.getBrain().remember(BrainMemoryKeys.NEAREST_ATTACKABLE_TARGET, targets.get(0));
+		// If we already have a target, re-validate the target.
+		if (nearestAttackableTargetOptional.isPresent()) {
+			Entity nearestAttackableTarget = nearestAttackableTargetOptional.get();
+
+			// Return early if the current target is still valid.
+			if (nearestAttackableTarget instanceof EntityLivingBase &&
+					this.isTargetValid((EntityLivingBase)nearestAttackableTarget)
+			) {
+				ctx.getBrain().remember(BrainMemoryKeys.NEAREST_ATTACKABLE_TARGET, nearestAttackableTarget);
+				return;
 			} else {
 				ctx.getBrain().forget(BrainMemoryKeys.NEAREST_ATTACKABLE_TARGET);
 			}
 		}
+
+		// Otherwise, scan for new targets.
+		if (livingEntitiesOptional.isPresent()) {
+			List<EntityLivingBase> livingEntities = livingEntitiesOptional.get();
+			List<EntityLivingBase> targets = livingEntities.stream().filter(
+					target -> this.isTargetValid(target) &&
+							ctx.getEntity().canEntityBeSeen(target)
+			).collect(Collectors.toList());
+
+			if (!targets.isEmpty()) {
+		        ctx.getBrain().remember(BrainMemoryKeys.NEAREST_ATTACKABLE_TARGET, targets.get(0));
+			}
+		}
+	}
+
+	private boolean isTargetValid(EntityLivingBase target) {
+		return !target.isDead &&
+				target.getHealth() > 0 &&
+				targetPredicate.test(target);
 	}
 }
