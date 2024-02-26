6c5c23ceab3cd2483fb4c7a09fc97c37cb45da8d
bvanseg <bvanseg@gmail.com>
2024-02-26 12:24:19 -0500

    Ported over trilobite (baby) entity (WIP).
    Signed-off-by: = <bvanseg@gmail.com>

---
+++ src/main/java/org/avp/client/render/entity/TrilobiteBabyRenderer.java
@@ -0,0 +1,22 @@
+package org.avp.client.render.entity;
+
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.util.ResourceLocation;
+import org.avp.AVPResources;
+import org.avp.client.model.entity.TrilobiteBabyModel;
+import org.avp.common.entity.TrilobiteBabyEntity;
+
+public class TrilobiteBabyRenderer extends MobRenderer<TrilobiteBabyEntity, TrilobiteBabyModel> {
+
+    private static final ResourceLocation TEXTURE = AVPResources.entityTextureLocation("trilobite");
+
+    public TrilobiteBabyRenderer(EntityRendererManager entityRendererManager) {
+        super(entityRendererManager, new TrilobiteBabyModel(), 0F);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(TrilobiteBabyEntity entity) {
+        return TEXTURE;
+    }
+}
--- src/main/java/org/avp/common/entity/AVPEntities.java
+++ src/main/java/org/avp/common/entity/AVPEntities.java
@@ -36,6 +36,7 @@ public class AVPEntities implements AVPRegistry {
     private static final RegistryObject<EntityType<OctohuggerEntity>> OCTOHUGGER = registerEntityType("octohugger", EntityType.Builder.create(OctohuggerEntity::new, EntityClassification.MONSTER).size(0.5F, 0.5F));
     private static final RegistryObject<EntityType<OvamorphEntity>> OVAMORPH = registerEntityType("ovamorph", EntityType.Builder.create(OvamorphEntity::new, EntityClassification.MONSTER).size(0.98F, 0.98F));
     private static final RegistryObject<EntityType<OvamorphDracoEntity>> OVAMORPH_DRACO = registerEntityType("ovamorph_draco", EntityType.Builder.create(OvamorphDracoEntity::new, EntityClassification.MONSTER).size(0.98F, 0.98F));
+    private static final RegistryObject<EntityType<TrilobiteBabyEntity>> TRILOBITE_BABY = registerEntityType("trilobite_baby", EntityType.Builder.create(TrilobiteBabyEntity::new, EntityClassification.MONSTER).size(0.98F, 0.98F));
     private static final RegistryObject<EntityType<UltramorphEntity>> ULTRAMORPH = registerEntityType("ultramorph", EntityType.Builder.create(UltramorphEntity::new, EntityClassification.MONSTER).size(0.98F, 2.98F));
 
     private static <T extends Entity> RegistryObject<EntityType<T>> registerEntityType(String name, EntityType.Builder<T> entityTypeBuilder) {
