e46d415cd3791eb2d3761abdc0dac08cbcc324e0
bvanseg <bvanseg@gmail.com>
2024-02-29 12:59:35 -0500

    Ported over engineer ship block items.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/AVPCommon.java
+++ common/src/main/java/org/avp/common/AVPCommon.java
@@ -1,5 +1,6 @@
 package org.avp.common;
 
+import org.avp.common.block.AVPEngineerBlocks;
 import org.avp.common.item.*;
 
 /**
@@ -13,5 +14,7 @@ public class AVPCommon {
         AVPFoodItems.getInstance().register();
         AVPItems.getInstance().register();
         AVPWeaponItems.getInstance().register();
+
+        AVPEngineerBlocks.getInstance().register();
     }
 }
