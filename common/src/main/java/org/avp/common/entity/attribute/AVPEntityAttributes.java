d99e1ef2599e72a0274a77bccf255eb3054070c8
bvanseg <bvanseg@gmail.com>
2024-03-03 15:18:21 -0500

    Corrected all entity attribute values.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/entity/attribute/AVPEntityAttributes.java
+++ common/src/main/java/org/avp/common/entity/attribute/AVPEntityAttributes.java
@@ -30,19 +30,69 @@ public class AVPEntityAttributes {
         return BINDINGS;
     }
 
-    public static final AttributeSupplier BELUGABURSTER = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier BELUGAMORPH = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier DEACON_ADULT = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier DEACON_ADULT_ENGINEER = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier DRACOBURSTER = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier DRACOMORPH = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier ENGINEER = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier OCTOHUGGER = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier OVAMORPH = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier OVAMORPH_DRACO = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier TRILOBITE = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier TRILOBITE_BABY = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
-    public static final AttributeSupplier ULTRAMORPH = Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 20).add(Attributes.MOVEMENT_SPEED, 1).add(Attributes.FOLLOW_RANGE, 1).build();
+    private static AttributeSupplier.Builder createAvPAttributes() {
+        return Monster.createMonsterAttributes()
+            .add(Attributes.ARMOR, 0)
+            .add(Attributes.FOLLOW_RANGE, 16)
+            .add(Attributes.MAX_HEALTH, 20)
+            .add(Attributes.MOVEMENT_SPEED, 1);
+    }
+
+    public static final AttributeSupplier BELUGABURSTER = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 0.5D)
+        .add(Attributes.MAX_HEALTH, 14.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.6499999761581421D).build();
+    public static final AttributeSupplier BELUGAMORPH = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 4.5D)
+        .add(Attributes.MAX_HEALTH, 100.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.4700000238418579D).build();
+    public static final AttributeSupplier DEACON_ADULT = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 3D)
+        .add(Attributes.MAX_HEALTH, 120.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.4700000238418579D).build();
+    public static final AttributeSupplier DEACON_ADULT_ENGINEER = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 4D)
+        .add(Attributes.MAX_HEALTH, 150.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.4700000238418579D).build();
+    public static final AttributeSupplier DRACOBURSTER = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 0.5D)
+        .add(Attributes.MAX_HEALTH, 25.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.6499999761581421D).build();
+    public static final AttributeSupplier DRACOMORPH = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 8.0D)
+        .add(Attributes.KNOCKBACK_RESISTANCE, 0.75D)
+        .add(Attributes.MAX_HEALTH, 400.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.45199999761581421D).build();
+    public static final AttributeSupplier ENGINEER = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 8.0D)
+        .add(Attributes.KNOCKBACK_RESISTANCE, 0.85D)
+        .add(Attributes.MAX_HEALTH, 160.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.5199999761581421D).build();
+    public static final AttributeSupplier OCTOHUGGER = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 0.50D)
+        .add(Attributes.MAX_HEALTH, 14.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.55D).build();
+    public static final AttributeSupplier OVAMORPH = createAvPAttributes()
+        .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D)
+        .add(Attributes.MAX_HEALTH, 8.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.0D).build();
+    public static final AttributeSupplier OVAMORPH_DRACO = createAvPAttributes()
+        .add(Attributes.KNOCKBACK_RESISTANCE, 1.0D)
+        .add(Attributes.MAX_HEALTH, 16.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.0D).build();
+    public static final AttributeSupplier TRILOBITE = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 4.0D)
+        .add(Attributes.KNOCKBACK_RESISTANCE, 1F)
+        .add(Attributes.MAX_HEALTH, 44.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.6999999761581421D).build();
+    public static final AttributeSupplier TRILOBITE_BABY = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 0.5D)
+        .add(Attributes.MAX_HEALTH, 16.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.6499999761581421D).build();
+    public static final AttributeSupplier ULTRAMORPH = createAvPAttributes()
+        .add(Attributes.ATTACK_DAMAGE, 5.5D)
+        .add(Attributes.MAX_HEALTH, 230.0D)
+        .add(Attributes.MOVEMENT_SPEED, 0.4700000238418579D).build();
 
     private static <T extends Mob> void addBinding(GameObject<EntityType<T>> gameObject, AttributeSupplier attributeSupplier) {
         BINDINGS.add(new AbstractMap.SimpleEntry<>(gameObject, attributeSupplier));
