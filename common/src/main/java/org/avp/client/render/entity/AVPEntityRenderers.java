7d31f00087b732142f3a59f0d5aa4029790c4067
bvanseg <bvanseg@gmail.com>
2024-03-07 01:34:14 -0500

    Ported over belugamorph entity.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/client/render/entity/AVPEntityRenderers.java
+++ common/src/main/java/org/avp/client/render/entity/AVPEntityRenderers.java
@@ -1,7 +1,9 @@
 package org.avp.client.render.entity;
 
 import org.avp.client.model.entity.living.BelugabursterModel;
+import org.avp.client.model.entity.living.BelugamorphModel;
 import org.avp.client.render.entity.living.BelugabursterRenderer;
+import org.avp.client.render.entity.living.BelugamorphRenderer;
 import org.avp.client.util.EntityModelLayerData;
 import org.avp.client.util.EntityRenderData;
 import org.avp.common.entity.AVPEntityTypes;
@@ -22,6 +24,15 @@ public class AVPEntityRenderers {
                 BelugabursterRenderer::new,
                 List.of(
                     new EntityModelLayerData(BelugabursterModel.LAYER_LOCATION, BelugabursterModel::createBodyLayer)
+                )
+            )
+        );
+        AVPEntityRenderRegistry.addBinding(
+            new EntityRenderData<>(
+                AVPEntityTypes.BELUGAMORPH,
+                BelugamorphRenderer::new,
+                List.of(
+                    new EntityModelLayerData(BelugamorphModel.LAYER_LOCATION, BelugamorphModel::createBodyLayer)
                 )
             )
         );
---
+++ common/src/main/java/org/avp/client/render/entity/living/BelugamorphRenderer.java
@@ -0,0 +1,26 @@
+package org.avp.client.render.entity.living;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import org.avp.client.model.entity.living.BelugamorphModel;
+import org.avp.common.AVPResources;
+import org.avp.common.entity.living.Belugamorph;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Boston Vanseghi
+ */
+public class BelugamorphRenderer extends MobRenderer<Belugamorph, BelugamorphModel> {
+
+    private static final ResourceLocation TEXTURE = AVPResources.entityTextureLocation("belugamorph");
+
+    public BelugamorphRenderer(EntityRendererProvider.Context context) {
+        super(context, new BelugamorphModel(context.bakeLayer(BelugamorphModel.LAYER_LOCATION)), 0.5f);
+    }
+
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull Belugamorph entity) {
+        return TEXTURE;
+    }
+}
