849e392819b3bde283d606ef157ca8f27ba1b105
bvanseg <bvanseg@gmail.com>
2024-03-20 08:45:35 -0400

    Cleanup.
    Signed-off-by: = <bvanseg@gmail.com>

--- fabric/src/main/java/org/avp/fabric/AVPFabric.java
+++ fabric/src/main/java/org/avp/fabric/AVPFabric.java
@@ -3,20 +3,17 @@ package org.avp.fabric;
 import net.fabricmc.api.ModInitializer;
 
 import org.avp.common.AVPCommon;
-import org.avp.common.AVPConstants;
 import org.avp.fabric.common.worldgen.AVPFabricWorldGenFeatures;
 import org.avp.fabric.service.FabricEntityAttributeRegistry;
 
+/**
+ * @author Boston Vanseghi
+ */
 public class AVPFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // This method is invoked by the Fabric mod loader when it is ready
-        // to load your mod. You can access Fabric and Common code in this
-        // project.
-
         // Use Fabric to bootstrap the Common mod.
-        AVPConstants.LOGGER.info("Hello Fabric world!");
         AVPCommon.init();
         AVPFabricWorldGenFeatures.getInstance().register();
         FabricEntityAttributeRegistry.getInstance().register();
