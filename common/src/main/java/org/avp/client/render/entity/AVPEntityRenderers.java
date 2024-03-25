c6ed079c0c3f4e526dc04f120a815a9a493da2ec
bvanseg <bvanseg@gmail.com>
2024-03-25 07:54:13 -0400

    Port belugaburster to azurelib geo model format.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/client/render/entity/AVPEntityRenderers.java
+++ common/src/main/java/org/avp/client/render/entity/AVPEntityRenderers.java
@@ -1,14 +1,11 @@
 package org.avp.client.render.entity;
 
-import java.util.List;
-
-import org.avp.client.model.entity.living.BelugabursterModel;
-import org.avp.client.model.entity.living.BelugamorphModel;
 import org.avp.client.render.entity.living.BelugabursterRenderer;
 import org.avp.client.render.entity.living.BelugamorphRenderer;
-import org.avp.client.util.EntityModelLayerData;
 import org.avp.client.util.EntityRenderData;
 import org.avp.common.entity.AVPEntityTypes;
+
+import java.util.List;
 
 /**
  * @author Boston Vanseghi
@@ -22,9 +19,7 @@ public class AVPEntityRenderers {
             new EntityRenderData<>(
                 AVPEntityTypes.BELUGABURSTER,
                 BelugabursterRenderer::new,
-                List.of(
-                    new EntityModelLayerData(BelugabursterModel.LAYER_LOCATION, BelugabursterModel::createBodyLayer)
-                )
+                List.of()
             )
         );
         AVPEntityRenderRegistry.addBinding(
--- common/src/main/java/org/avp/client/render/entity/living/BelugabursterRenderer.java
+++ common/src/main/java/org/avp/client/render/entity/living/BelugabursterRenderer.java
@@ -1,27 +1,16 @@
 package org.avp.client.render.entity.living;
 
+import mod.azure.azurelib.common.api.client.renderer.GeoEntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.NotNull;
-
 import org.avp.client.model.entity.living.BelugabursterModel;
-import org.avp.common.AVPResources;
 import org.avp.common.entity.living.Belugaburster;
 
 /**
  * @author Boston Vanseghi
  */
-public class BelugabursterRenderer extends MobRenderer<Belugaburster, BelugabursterModel> {
-
-    private static final ResourceLocation TEXTURE = AVPResources.entityTextureLocation("belugaburster");
+public class BelugabursterRenderer extends GeoEntityRenderer<Belugaburster> {
 
     public BelugabursterRenderer(EntityRendererProvider.Context context) {
-        super(context, new BelugabursterModel(context.bakeLayer(BelugabursterModel.LAYER_LOCATION)), 0.5f);
-    }
-
-    @Override
-    public @NotNull ResourceLocation getTextureLocation(@NotNull Belugaburster entity) {
-        return TEXTURE;
+        super(context, new BelugabursterModel());
     }
 }
