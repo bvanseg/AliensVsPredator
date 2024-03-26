6df012181e9fe8091e6c96a544935cc40581d58c
bvanseg <bvanseg@gmail.com>
2024-03-26 07:50:54 -0400

    Added AK-47 (experimental).
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/org/avp/common/item/AVPWeaponItems.java
+++ common/src/main/java/org/avp/common/item/AVPWeaponItems.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
+import org.avp.common.item.impl.AK47Item;
 import org.avp.common.service.Services;
 import org.avp.common.util.GameObject;
 
@@ -72,7 +73,7 @@ public class AVPWeaponItems {
         GRENADE_M40 = register("grenade_m40", () -> new Item(new Item.Properties()));
 
         WEAPON_88MOD4_COMBAT_PISTOL = register("weapon_88mod4_combat_pistol", () -> new Item(new Item.Properties()));
-        WEAPON_AK_47 = register("weapon_ak_47", () -> new Item(new Item.Properties()));
+        WEAPON_AK_47 = register("weapon_ak_47", () -> new AK47Item(new Item.Properties()));
         WEAPON_M4_CARBINE = register("weapon_m4_carbine", () -> new Item(new Item.Properties()));
         WEAPON_M41A_PULSE_RIFLE = register("weapon_m41a_pulse_rifle", () -> new Item(new Item.Properties()));
         WEAPON_M56_SMARTGUN = register("weapon_m56_smartgun", () -> new Item(new Item.Properties()));
